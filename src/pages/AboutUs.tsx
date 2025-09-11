292dc0a7f9094be3ca813295eb185374fdb465b4
mohamedsoliman515 <mohamedelmoregy515@gmail.com>
2025-09-11 16:10:34 +0300

    Add some Info in Aboutus

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,7 +1,14 @@
 const AboutUs = () => {
   return (
     <div className=" d-flex align-items-center justify-content-center flex-column">
-
+      <h2>Hi I'm Mohamed Suliman</h2>
+      <p>
+        Results-driven Front-End Developer with 1 year of hands-on experience
+        crafting responsive, user friendly, and performant web applications.
+        Strong foundation in modern JavaScript frameworks, UI/UX best practices,
+        and performance optimization techniques. Committed to clean,
+        maintainable code and continuous improvement.
+      </p>
     </div>
   );
 };
